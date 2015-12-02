e0ccba19c26f04ef3641e3f7f7c414f7db129a7a
JVarga <josh.varga@gmail.com>
2015-12-01 21:24:18 -0600

    Adding unit test project and moving directory structure

---
+++ Blackjack.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
